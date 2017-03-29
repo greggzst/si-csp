1e069ea1c1f78f8c24ed3135acc8304fb7fded10
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-03-29 20:25:39 +0200

    Add constraints

--- Binary.java
+++ Binary.java
@@ -1,3 +1,7 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -20,6 +24,11 @@ public class Binary {
         }
 
         return true;
+    }
+
+    private boolean constrainsSatisfied(){
+        return areRowsUnique() && areColsUnique() && areOnlyTwoSymbolsNextToInCol()
+                && areOnlyTwoSymbolsNextToInRow() && isRowNumberOnesEqual() && isColNumberOnesEqual();
     }
 
     private boolean areOnlyTwoSymbolsNextToInRow(){
@@ -49,6 +58,33 @@ public class Binary {
             if(numOfElems == 3 || numOfElems == -3)
                 return false;
         }
+        return true;
+    }
+
+    private boolean areRowsUnique(){
+        for(int i = 0; i < board.length - 1; i++){
+            if(Arrays.equals(board[i],board[i+1]))
+                return false;
+        }
+
+        return true;
+    }
+
+    private boolean areColsUnique(){
+        List<int[]> columnList = new ArrayList<>();
+        for(int i = 0; i < board.length; i++){
+            int[] col = new int[board.length];
+            for(int j = 0; j < board.length; j++){
+                col[j] = board[j][i];
+            }
+            columnList.add(col);
+        }
+
+        for(int i = 0; i < columnList.size() - 1; i++){
+            if(Arrays.equals(columnList.get(i), columnList.get(i + 1)))
+                return false;
+        }
+
         return true;
     }
 
