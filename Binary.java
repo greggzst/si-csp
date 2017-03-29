ca9cc32d0767c00867e564520da8e5ff3ad4921a
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-03-29 14:01:27 +0200

    Add isSolved method

--- Binary.java
+++ Binary.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * Created by GreggJakubiak on 29.03.2017.
  */
@@ -14,5 +16,15 @@ public class Binary {
         }
     }
 
+    private boolean isSolved(){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(board[i][j] == -1)
+                    return false;
+            }
+        }
+
+        return true;
+    }
 
 }
