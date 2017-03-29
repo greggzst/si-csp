d20115d444dd3de43786e0c58674336c86dceac5
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-03-29 13:46:10 +0200

    Initialize empty puzzle with -1

--- Binary.java
+++ Binary.java
@@ -3,8 +3,16 @@
  */
 public class Binary {
     private int[][] board;
+    private int[] domain = {0, 1};
 
     public Binary(int n){
         board = new int[n][n];
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                board[i][j] = -1;
+            }
+        }
     }
+
+
 }
