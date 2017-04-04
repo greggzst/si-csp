017c452d4ef76185a2f4e1720c8f7fd60c8cacf8
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-04-04 14:55:54 +0200

    Chnage order

--- TripleComparator.java
+++ TripleComparator.java
@@ -6,6 +6,6 @@ import java.util.Comparator;
 public class TripleComparator implements Comparator<Triple>{
     @Override
     public int compare(Triple o1, Triple o2) {
-        return Integer.compare(o1.n,o2.n);
+        return Integer.compare(o2.n,o1.n);
     }
 }
