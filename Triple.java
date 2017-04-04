26419645816f5d53ab45be35896f52534a11cc19
Grzegorz Jakubiak <grzegorz.jakubiak@outlook.com>
2017-04-04 14:45:16 +0200

    Add helper class Triple

---
+++ Triple.java
@@ -0,0 +1,14 @@
+/**
+ * Created by GreggJakubiak on 04.04.2017.
+ */
+public class Triple {
+    public int x;
+    public int y;
+    public int n;
+
+    public Triple(int x, int y, int n){
+        this.x = x;
+        this.y = y;
+        this.n = n;
+    }
+}
